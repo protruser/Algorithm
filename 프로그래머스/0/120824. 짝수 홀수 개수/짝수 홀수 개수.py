5eeed3973a64c310efff5d60e613ceeb4d626070
protruser <137254772+protruser@users.noreply.github.com>
2024-09-11 16:34:02 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = [0,0]
+    for i in num_list:
+        if i%2 == 1:
+            answer[1] += 1
+        else:
+            answer[0] += 1
+            
+    return answer
+            
